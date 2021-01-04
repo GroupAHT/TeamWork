6a9094a0e1c971c5af1f9c41b595bb5b25488a4a
anlethanh048 <57203593+anlethanh048@users.noreply.github.com>
2021-01-04 12:48:02 +0700

    Create IUserService.cs

---
+++ Thuenhatro/ThuenhatroAPI/Services/IUserService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ThuenhatroAPI.Models;
+
+namespace ThuenhatroAPI.Services
+{
+    public interface IUserService
+    {
+        Task<IEnumerable<User>> GetUsers();
+        Task<User> GetUser(string id);
+        Task<User> PostUser(User user);
+        Task<User> PutUser(User user);
+        Task<User> DeleteUser(string id);
+
+    }
+}
