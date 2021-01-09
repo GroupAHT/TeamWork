ccf6f6d25c5a48bd08876e3b46edbbeef45f5993
anlethanh048 <57203593+anlethanh048@users.noreply.github.com>
2021-01-09 23:28:50 +0700

    Create CatagoryvsProperty.cs

---
+++ Thuenhatro/ThuenhatroClient/Models/CatagoryvsProperty.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ThuenhatroClient.Models;
+
+namespace ThuenhatroClient.Models
+{
+    public class CatagoryvsProperty
+    {
+        public Districks districks { get; set; }
+        public Ward ward { get; set; }
+    }
+}
