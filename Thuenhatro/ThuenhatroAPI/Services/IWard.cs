456c3a51eb7c814b8962b793c68340cc60dcbd86
anlethanh048 <57203593+anlethanh048@users.noreply.github.com>
2021-01-09 23:30:31 +0700

    Create IWard.cs

---
+++ Thuenhatro/ThuenhatroAPI/Services/IWard.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ThuenhatroAPI.Models;
+
+namespace ThuenhatroAPI.Service
+{
+   public interface IWard
+    {
+        Task<IEnumerable<Ward>> FindAll();
+    }
+}
