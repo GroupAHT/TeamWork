5d3e0115b578656b5eb86db6ee13069f758b708e
anlethanh048 <57203593+anlethanh048@users.noreply.github.com>
2021-01-09 23:30:37 +0700

    Create Warssds.cs

---
+++ Thuenhatro/ThuenhatroAPI/Services/Warssds.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ThuenhatroAPI.Models;
+using Microsoft.EntityFrameworkCore;
+namespace ThuenhatroAPI.Service
+{
+    public class Warssds : IWard
+    {
+        private Context context;
+        public Warssds(Context context)
+        {
+            this.context = context;
+        }
+        public async Task<IEnumerable<Ward>> FindAll()
+        {
+            return await context.ward.ToListAsync();
+        }
+    }
+}
