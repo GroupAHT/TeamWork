80f7e0a8e1af5c3bb6d5ab564f1373b9983cb9a2
anlethanh048 <57203593+anlethanh048@users.noreply.github.com>
2021-01-09 23:30:24 +0700

    Create Districk.cs

---
+++ Thuenhatro/ThuenhatroAPI/Services/Districk.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ThuenhatroAPI.Models;
+
+namespace ThuenhatroAPI.Service
+{
+    public class Districk : IDistrick
+    {
+        private Context context;
+        public Districk(Context context)
+        {
+            this.context = context;
+        }
+
+        public async Task<IEnumerable<Districks>> FindAll()
+        {
+            return await context.district.ToListAsync();
+        }
+    }
+}
