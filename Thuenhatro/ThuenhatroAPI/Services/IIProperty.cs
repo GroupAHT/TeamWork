6999a5dc6140b401a9a3c02ab82b94493f2d1973
anlethanh048 <57203593+anlethanh048@users.noreply.github.com>
2021-01-09 23:30:26 +0700

    Create IIProperty.cs

---
+++ Thuenhatro/ThuenhatroAPI/Services/IIProperty.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ThuenhatroAPI.Models;
+namespace ThuenhatroAPI.Service
+{
+   public interface IIProperty
+    {
+        Task<IEnumerable<Property>> GetAll();
+        Task<Property> AddNew(Property property);
+        Task<Property> Update(Property property);
+        Task<Property> Delete(int Id);
+        Task<IEnumerable<Catagory>> Getcta();
+        Task<Catagory> catagory(Catagory catagory);
+    }
+}
