e88cefe463be2ea29dea2f1b1fbf8f5d09048eec
anlethanh048 <57203593+anlethanh048@users.noreply.github.com>
2021-01-09 23:30:35 +0700

    Create Providerssd.cs

---
+++ Thuenhatro/ThuenhatroAPI/Services/Providerssd.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ThuenhatroAPI.Models;
+using Microsoft.EntityFrameworkCore;
+namespace ThuenhatroAPI.Service
+{
+    public class Providerssd : IProvider
+    {
+        private Context context;
+        public Providerssd(Context context)
+        {
+            this.context = context;
+        }
+
+        public async Task<IEnumerable<Provider>> FindAll()
+        {
+            return await context.province.ToListAsync();
+        }
+    }
+}
